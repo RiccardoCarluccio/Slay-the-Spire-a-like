b9cc0f838dd74e78bdf2e81ed230dc3e3b45100d
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-08-20 22:00:06 +0200

    push enemy deck

---
+++ javascript/functions/enemy-functions.js
@@ -0,0 +1,8 @@
+"use strict"
+
+function enemyTurn() {
+  for(let i=0; i<SPAWNED_ENEMY.actions; i++) {
+    const RANDOM_ACTION = Math.floor(Math.random() * SPAWNED_ENEMY_DECK.length + 1);
+    
+  }
+}
