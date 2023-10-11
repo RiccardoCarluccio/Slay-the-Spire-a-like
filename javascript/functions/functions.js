63e208d7753605286aaa62aab7f95560ea4b2257
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-10-11 20:20:02 +0200

    refactor display combo damage in hand

--- javascript/functions/functions.js
+++ javascript/functions/functions.js
@@ -71,34 +71,14 @@ function displayCards() {
   CARD_IN_HAND.innerHTML = "";
   for(let i=0; i<HAND.length; i++) {
     if(HAND[i].cardType === "attack") {
-      if(HAND[i].cardType === "attack" && rightSyncro && HAND[i].rightSyncroTrue) {
-        CARD_IN_HAND.innerHTML += `
-          <div id="card-in-hand-${HAND[i].indexCardCounter}" class="card card-in-hand ${HAND[i].cardType}" data-card-index="${HAND[i].indexCardCounter}" draggable="true" ondragstart="drag(event)">
-            <div class="card-energy attack-energy">${HAND[i].energyCost}</div>
-            <div class="card-name ${HAND[i].cardType}">${HAND[i].cardName}</div>
-            <div class="card-img"></div>
-            <div class="card-description ${HAND[i].cardType}">Damage: ${(calculateDamage(HAND[i]) + HAND[i].rightSyncroDamage)} <br> ${HAND[i].description}</div>
-          </div>
-        `;
-      } if(HAND[i].cardType === "attack" && HAND[i].stanceCombo === APERTURA_CHAR.stance) {
-        CARD_IN_HAND.innerHTML += `
-          <div id="card-in-hand-${HAND[i].indexCardCounter}" class="card card-in-hand ${HAND[i].cardType}" data-card-index="${HAND[i].indexCardCounter}" draggable="true" ondragstart="drag(event)">
-            <div class="card-energy attack-energy">${HAND[i].energyCost}</div>
-            <div class="card-name ${HAND[i].cardType}">${HAND[i].cardName}</div>
-            <div class="card-img"></div>
-            <div class="card-description ${HAND[i].cardType}">Damage: ${(calculateDamage(HAND[i]) + HAND[i].stanceDamageBonus)} <br> ${HAND[i].description}</div>
-          </div>
-        `;
-      } else {
-        CARD_IN_HAND.innerHTML += `
-          <div id="card-in-hand-${HAND[i].indexCardCounter}" class="card card-in-hand ${HAND[i].cardType}" data-card-index="${HAND[i].indexCardCounter}" draggable="true" ondragstart="drag(event)">
-            <div class="card-energy attack-energy">${HAND[i].energyCost}</div>
-            <div class="card-name ${HAND[i].cardType}">${HAND[i].cardName}</div>
-            <div class="card-img"></div>
-            <div class="card-description ${HAND[i].cardType}">Damage: ${calculateDamage(HAND[i])} <br> ${HAND[i].description}</div>
-          </div>
-        `;
-      }
+      CARD_IN_HAND.innerHTML += `
+        <div id="card-in-hand-${HAND[i].indexCardCounter}" class="card card-in-hand ${HAND[i].cardType}" data-card-index="${HAND[i].indexCardCounter}" draggable="true" ondragstart="drag(event)">
+          <div class="card-energy attack-energy">${HAND[i].energyCost}</div>
+          <div class="card-name ${HAND[i].cardType}">${HAND[i].cardName}</div>
+          <div class="card-img"></div>
+          <div class="card-description ${HAND[i].cardType}">Damage: ${(calculateDamage(HAND[i]) + checkCombo(HAND[i]))} <br> ${HAND[i].description}</div>
+        </div>
+      `;
     } else if (HAND[i].cardType === "block") {
       CARD_IN_HAND.innerHTML += `
         <div id="card-in-hand-${HAND[i].indexCardCounter}" class="card card-in-hand ${HAND[i].cardType}" data-card-index="${HAND[i].indexCardCounter}" draggable="true" ondragstart="drag(event)">
@@ -110,6 +90,16 @@ function displayCards() {
       `;
     }
     displayOutline(i);
+  }
+}
+
+function checkCombo(card) {
+  if(card.cardType === "attack" && rightSyncro && card.rightSyncroTrue) {
+    return card.rightSyncroDamage;
+  } else if (card.cardType === "attack" && card.stanceCombo === APERTURA_CHAR.stance) {
+    return card.stanceDamageBonus
+  } else {
+    return 0;
   }
 }
 
