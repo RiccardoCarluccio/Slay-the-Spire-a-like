aa8a782ed913930ae7a244a13c9ff9aeb6229988
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-11-20 00:26:45 +0100

    do close offcanva

--- javascript/menu-navigation.js
+++ javascript/menu-navigation.js
@@ -2,10 +2,10 @@
 
 const DISCARD_PILE_CONTAINER = document.getElementById("discard-pile");                   //contenitore
 const DISCARD_PILE_OFFCANVA = document.getElementById("opened-discard-pile");
-//const CLOSE_DISCARD_PILE = document.getElementById("close-discard-pile");               //pulsante
+const CLOSE_DISCARD_PILE = document.getElementById("close-discard-pile");               //pulsante
 
 DISCARD_PILE_CONTAINER.addEventListener("click", openDiscardPileFunction);
-//CLOSE_DISCARD_PILE.addEventListener("click", closeDiscardPileFunction);
+CLOSE_DISCARD_PILE.addEventListener("click", closeDiscardPileFunction);
 
 function openDiscardPileFunction() {
   DISCARD_PILE_OFFCANVA.style.display = "block";
